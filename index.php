6575d4bc46e4a4c37d7689110939b15b3ff635f2
Valkyrux <philadephos.granata@gmail.com>
2022-01-28 16:38:35 +0100

    init

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+$text = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus exercitationem qui quidem obcaecati! Odio nulla distinctio dolor quibusdam, maiores debitis sapiente fuga labore esse itaque saepe minima accusamus ut. Ipsa!";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>bad_words</title>
+</head>
+
+<body>
+    <h2>Testo inserito da variabile:</h2>
+    <p>
+        <?= $text ?>
+    </p>
+</body>
+
+</html>
