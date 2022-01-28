7ff39ff12c08ff6b29f6c4d5334956940ff699e8
Valkyrux <philadephos.granata@gmail.com>
2022-01-28 17:51:22 +0100

    fine-esercizio

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
-$text = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus exercitationem qui quidem obcaecati! Odio nulla distinctio dolor quibusdam, maiores debitis sapiente fuga labore esse itaque saepe minima accusamus ut. Ipsa!";
-
+$text = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum dolor sit amet consectetur, adipisicing elit.";
+$input_text = $_GET["input"];
+// rimpiazzo tutte le lettere dell'input con degli * in modo tale da avere tanti asterischi quanta la parola data
+$censured_word = str_replace(str_split($input_text), "*", $input_text);
+// sostituisco all'input la parola censurata nel testo, utilizzo str_ireplace piuttosto che str_replace così la rendo casa-insensitive
+$new_text = str_ireplace($input_text, $censured_word, $text);
 ?>
 
 <!DOCTYPE html>
@@ -17,6 +21,10 @@ $text = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus exercit
     <h2>Testo inserito da variabile:</h2>
     <p>
         <?= $text ?>
+    </p>
+    <h2>Testo con <?= $_GET["input"] ?> censurato:</h2>
+    <p>
+        <?= $new_text ?>
     </p>
 </body>
 
